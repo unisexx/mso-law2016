018a4a4ad1c17ead283961dd2b130687f8c14ae0
unisexx <unisexx@gmail.com>
2016-08-31 14:27:10 +0700

    ....

--- application/modules/admin/views/law_datalaws/index.php
+++ application/modules/admin/views/law_datalaws/index.php
@@ -69,7 +69,7 @@
 	  <!-- <td>-</td> -->
 	  <td><?=get_datalaw_status($row->status)?></td>
 	  <td>
-	  	<?if((user_login()->user_group_id == 1) || (user_login()->user_group_id == $row->created_user_group_id)):?>
+	  	<?if((user_login()->user_group_id == 1) || (user_login()->user_group_id == 2) || (user_login()->user_group_id == $row->created_user_group_id)):?>
 	  	<a href="admin/law_datalaws/form/<?=$row->id?>"><img src="themes/admin/images/edit.png" width="24" height="24" style="margin-right:10px;" class="vtip" title="แก้ไขรายการนี้" /></a> <a href="admin/law_datalaws/delete/<?=$row->id?>"><img src="themes/admin/images/remove.png" width="32" height="32" class="vtip" title="ลบรายการนี้"  onclick="return confirm('<?php echo lang('notice_confirm_delete');?>')" /></a>
 	  	<?endif;?>
 	  </td>
