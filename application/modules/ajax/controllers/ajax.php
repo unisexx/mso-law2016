f85c3744a2dc11d68a9dba9a9a2d71143133f708
Aut <ultraauchz@gmail.com>
2016-10-11 10:41:45 +0700

    update function get_select_apply_power_id > select order by name_th

--- application/modules/ajax/controllers/ajax.php
+++ application/modules/ajax/controllers/ajax.php
@@ -5,19 +5,19 @@ Class Ajax extends Public_Controller
 	{
 		parent::__construct();
 	}
-	
+
 	function get_select_submaintype(){
 		if($_GET){
 			echo form_dropdown('law_submaintype_id', get_option('id','typeName','law_submaintypes where law_maintype_id = '.$_GET['law_maintype_id'].' order by id asc'), @$_GET['law_submaintype_id'],'class="form-control" style="width:auto;"','--- เลือกประเภทย่อยกฎหมาย ---');
 		}
 	}
-	
+
 	function get_select_lawtype(){
 		if($_GET){
 			echo form_dropdown('law_type_id', get_option('id','name','law_types where law_group_id = '.$_GET['law_group_id'].' order by id asc'), @$_GET['law_type_id'],'class="form-control" style="width:auto;"','--- เลือกหมวดกฎหมาย ---');
 		}
 	}
-	
+
 	function get_law_group_list_data(){
 		if($_GET){
 			// function นี้เหมือนก้อบมาจาก law/group_list
@@ -30,18 +30,18 @@ Class Ajax extends Public_Controller
 			$this->load->view('get_law_group_list_data',$data);
 		}
 	}
-	
+
 	function get_law_data(){
 		if($_GET){
 			$condition = " 1=1 ";
 			if(@$_GET['search']){$condition .= ' and name_th LIKE "%'.$_GET['search'].'%" ';}
-			
+
 			$sql = "select id, name_th, status from law_datalaws where ".$condition." order by id desc";
         	$data['rs'] = $this->db->query($sql)->result();
 			$this->load->view('get_law_data',$data);
 		}
 	}
-	
+
 	function get_cross_law_data(){
 		if($_GET){
 			$condition = " 1=1 ";
@@ -50,18 +50,18 @@ Class Ajax extends Public_Controller
 			if(@$_GET['law_type_id']){$condition .= ' and law_type_id = '.$_GET['law_type_id'];}
 			if(@$_GET['law_maintype_id']){$condition .= ' and law_maintype_id = '.$_GET['law_maintype_id'];}
 			if(@$_GET['law_submaintype_id']){$condition .= ' and law_submaintype_id = '.$_GET['law_submaintype_id'];}
-			
+
 			$sql = "select id, name_th, law_group_id, law_type_id, law_maintype_id, law_submaintype_id, status from law_datalaws where ".$condition." order by id desc";
         	$data['rs'] = $this->db->query($sql)->result();
 			$this->load->view('get_cross_law_data',$data);
 		}
 	}
-	
+
 	function get_related_law_data(){
 		if($_GET){
 			$condition = " 1=1 ";
 			if(@$_GET['search']){$condition .= ' and name_th LIKE "%'.$_GET['search'].'%" ';}
-			
+
 			$sql = "select id, name_th, status from law_datalaws where ".$condition." order by id desc";
         	$data['rs'] = $this->db->query($sql)->result();
 			$this->load->view('get_related_law_data',$data);
@@ -76,19 +76,19 @@ Class Ajax extends Public_Controller
 			$rs->save();
 		}
 	}
-	
+
 	function get_select_power_group(){
 		if($_GET){
 			echo form_dropdown('apply_power_group',get_option('id','typeName','law_submaintypes where id < '.$_GET['law_submaintype_id'].' order by id asc'),@$_GET['apply_power_group'],'class="form-control" style="width:auto;"','-- เลือกประเภทกฎหมายย่อยที่อาศัยอำนาจ --');
 		}
 	}
-	
+
 	function get_select_apply_power_id(){
 		if($_GET){
-			$sql = "select id, name_th from law_datalaws where law_submaintype_id = ".$_GET['apply_power_group']." order by id asc";
+			$sql = "select id, name_th from law_datalaws where law_submaintype_id = ".$_GET['apply_power_group']." order by name_th asc";
 			$rs = $this->db->query($sql)->result();
 			// echo $sql;
-			
+
 			echo "<select name='apply_power_id' class='form-control' style='width:500px;'>";
 			echo "<option value=''>--- กรุณาเลือกกฎหมายที่อาศัยอำนาจ ---</option>";
 			foreach($rs as $row):
@@ -96,7 +96,7 @@ Class Ajax extends Public_Controller
 				echo "<option value='".$row->id."' ".$select.">".str_replace("|"," ",$row->name_th)."</option>";
 			endforeach;
 			echo "</select>";
-			
+
 			// echo form_dropdown('law_type_id', get_option('id','name_th','law_datalaws where law_submaintype_id = '.$_GET['apply_power_group'].' order by id asc'), @$_GET['apply_power_id'],'class="form-control" style="width:500px;"','--- กรุณาเลือกกฎหมายที่อาศัยอำนาจ ---');
 		}
 	}
